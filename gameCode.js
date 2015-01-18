1b3c78f093dccb637d24959151e8e174f7c6288b
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-17 16:24:54 -1000

    add marisa, punching bag; slow down bullet spam a little

--- gameCode.js
+++ gameCode.js
@@ -1,8 +1,11 @@
     window.onload = function() {
   //    var cursors, fireButton, bombButton;
 
-      var reimu, reimuBullets;
+      var reimu, reimuBullets, reimu_hp = 100;
       var lastDirection = [0,-1];
+      var reimuLastFired = 0;
+
+      var marisa, marisa_hp = 100;
 
       var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
         preload: preload,
@@ -14,7 +17,9 @@
 
         //            game.load.image('logo', 'phaser.png');
         game.load.image('reimu', 'img/reimu.png');
-        game.load.image('yinyang', 'img/yinyang.png');
+        game.load.image('yinyang', 'img/yinyang.png?foo');
+        game.load.image('reimu-bullet', 'img/reimu-bullet.png');
+        game.load.image('marisa', 'img/marisa.png');
       }
 
       function update() {
@@ -42,15 +47,24 @@
             lastDirection = [0, 1];
           }
 
-          if (fireButton.isDown) {
+          if (fireButton.isDown && (reimuLastFired + 300 < game.time.now)) {
+
             var bullet = reimuBullets.getFirstExists(false);
-
             if (bullet) {
-                bullet.reset(reimu.x, reimu.y);
-              bullet.body.velocity.x = lastDirection[0] * movespeed * 1.5;
-              bullet.body.velocity.y = lastDirection[1] * movespeed * 1.5;
-
+                bullet.reset(reimu.x + lastDirection[1]*15, 
+                  reimu.y + lastDirection[0] * 15);
+                bullet.body.velocity.x = lastDirection[0] * movespeed * 1.5;
+                bullet.body.velocity.y = lastDirection[1] * movespeed * 1.5;
             }
+            bullet = reimuBullets.getFirstExists(false);
+            if (bullet) {
+                bullet.reset(reimu.x - lastDirection[1]*15, 
+                  reimu.y - lastDirection[0] * 15);
+                bullet.body.velocity.x = lastDirection[0] * movespeed * 1.5;
+                bullet.body.velocity.y = lastDirection[1] * movespeed * 1.5;
+            }
+
+            reimuLastFired = game.time.now;
 
           }
 
@@ -63,16 +77,21 @@
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         reimu = game.add.sprite(game.world.centerX, game.world.centerY, 'reimu');
+        reimu.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(reimu);
 
         reimuBullets = game.add.group();
         reimuBullets.enableBody = true;
         reimuBullets.physicsBodyType = Phaser.Physics.ARCADE;
-        reimuBullets.createMultiple(30, 'yinyang');
+        reimuBullets.createMultiple(30, 'reimu-bullet');
         reimuBullets.setAll('anchor.x', 0.5);
         reimuBullets.setAll('anchor.y', 1);
         reimuBullets.setAll('outOfBoundsKill', true);
         reimuBullets.setAll('checkWorldBounds', true);
+
+        marisa = game.add.sprite(600, 600, 'marisa');
+        marisa.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(marisa);
 
         cursors = game.input.keyboard.createCursorKeys();
         fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
